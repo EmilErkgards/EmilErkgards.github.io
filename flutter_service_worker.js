4d6fcfe6400525c2463e145d601ac043b73ca0ec
Emil <emilerkgards@hotmail.com>
2023-05-20 08:39:25 +0200

    Adding guests should work

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"apple-app-site-association": "0abfeeacc16349f3092a8f791d9b25
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3234784fc555d1038c0d35e1472ac52a",
-"/": "3234784fc555d1038c0d35e1472ac52a",
-"main.dart.js": "c88f7c3abfec197463bf2c9874a25a8d",
+"index.html": "a3b7c25c06876afb22f032517c5b79fb",
+"/": "a3b7c25c06876afb22f032517c5b79fb",
+"main.dart.js": "5d9d197fd2898e8db9935369747e432d",
 "manifest.json": "96abac8054dfe909f6793dc27dca115c",
 "version.json": "b3641041e820499ab1cad07b4634ef16"};
 // The application shell files that are downloaded before a service worker can
